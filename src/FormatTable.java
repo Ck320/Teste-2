b181f2ccaaa778884c4700c6d654928d4fdd7180
Ck320 <mauricio.s.polloni@gmail.com>
2020-08-03 22:23:57 -0300

    funções de extração de tabela concluida

--- src/FormatTable.java
+++ src/FormatTable.java
@@ -1,17 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FormatTable
 {
-	String result;
+	private String result;
 	public FormatTable(String text,int startLine,int stopLine)
 	{
-		String[] line = text.split("\n");
+		String[] line = text.split("\n");		
 		
+		List<String> lineList = new ArrayList<String>();
 		for(int i=startLine;i<=stopLine;i++)
 		{
-			System.out.println(line[i]);
+			line[i]=line[i].substring(0,line[i].length()-2);
+			if(line[i].equals("Padrão TISS - Componente Organizacional – junho de 2020"));
+			else if(line[i].isBlank());
+			else lineList.add(line[i]);			
 		}
-	}
+		
+		for(int i=0;i<lineList.size();i++)
+		{
+			if(lineList.get(i).length()<3)lineList.remove(i);
+			if(lineList.get(i).length()==3)
+			{
+				lineList.set(i,lineList.get(i)+" "+lineList.get(i+1)+" "+lineList.get(i+2));
+				lineList.remove(i+1);
+				lineList.remove(i+1);					
+			}
+			String[]aux=lineList.get(i).split(" ",2);
+			
+			System.out.println(aux[0]+"; "+aux[1]+";");
+		}		
+	}	
 	
-	public String getResult() {
+	public String getResult() {		
 		return result;
 	}
 	
--- src/Teste_2.java
+++ src/Teste_2.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 public class Teste_2 {
 	public static void main(String[] args) throws IOException {
 		File x = new File("teste.pdf");
-		ExtractTextByPages text = new ExtractTextByPages(x, 81, 86);
-		new FormatTable(text.getText(),6,11);
+		ExtractTextByPages text = new ExtractTextByPages(x, 81, 87);
+		new FormatTable(text.getText(),32,197);
 	}
 
 }
