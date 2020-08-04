077761b9f6ce36a5b6065ae9a8c37784225c75d5
Mauricio Sugimoto <65788187+Ck320@users.noreply.github.com>
2020-08-04 12:08:16 -0300

    Update Teste_2.java
    adicionando comentários extras

--- src/Teste_2.java
+++ src/Teste_2.java
@@ -2,9 +2,9 @@ import java.io.File;
 import java.io.IOException;
 
 /*
- * @author Maur�cio Sugimoto Polloni
+ * @author Maurício Sugimoto Polloni
  * 
- * Classe principal, responsavel por organizar a ordem das fun��es
+ * Classe principal, responsavel por organizar a ordem das funções
  *
  */
 
@@ -14,9 +14,9 @@ public class Teste_2 {
 		File x = new File("Padrao_TISS_Componente_Organizacional_202006.pdf");		
 		ExtractTextByPages text = new ExtractTextByPages(x, 81, 87); // as tabelas se encontram entre as pagicas 81 e 87
 		
-		FormatTable quadro30 = new FormatTable(text.getText(),6,11);
-		FormatTable quadro31 = new FormatTable(text.getText(),32,197);
-		FormatTable quadro32 = new FormatTable(text.getText(),238,241);
+		FormatTable quadro30 = new FormatTable(text.getText(),6,11); 	//as informações do quadro 30 se encontram entre as linhas 6 e 11 do texto extraido
+		FormatTable quadro31 = new FormatTable(text.getText(),32,197);	//as informações do quadro 31 se encontram entre as linhas 32 e 197 do texto extraido
+		FormatTable quadro32 = new FormatTable(text.getText(),238,241);	//as informações do quadro 32 se encontram entre as linhas 238 e 241 do texto extraido
 		
 		new CreateFile("quadro30", quadro30.getResult());
 		new CreateFile("quadro31", quadro31.getResult());
